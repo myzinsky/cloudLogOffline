b288d68b7b8537f0b153b1c05a36e7b96a919f89
Kai Pastor <dg0yt@darc.de>
2024-05-01 10:12:49 +0200

    Manage translations artifacts with qmake

--- src/translationmanager.cpp
+++ src/translationmanager.cpp
@@ -19,7 +19,7 @@ void translationManager::switchToLanguage(const QString &language)
     if(success == true) { // Search for iOS:
         qDebug() << "Language Switched iOS: " << language;
     } else { // Search for Android again:
-        success = translator->load(":/translations/"+language+".qm");
+        success = translator->load(":/i18n/"+language+".qm");
         if(success == true) {
             qDebug() << "Language Switched Android: " << language;
         } else {
